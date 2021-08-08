4879a257b73f731920fd0df51b49c03812f93f6a
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-08 18:33:14 +0530

    TC-2.1

--- MoodAnalyserTesting.java
+++ MoodAnalyserTesting.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class MoodAnalyserTesting {
 
     @Test
+    //If message contains SAD then it will show SAD
     public void givenSadMessage_WhenAnalysed_MessageShouldReturnSad() {
         MoodAnalyser checkMood = new MoodAnalyser("I am in Sad Mood");
         String mood = checkMood.analyseMood();
@@ -13,10 +14,18 @@ public class MoodAnalyserTesting {
     }
 
     @Test
+    //If message doesn't contains SAD then it will show HAPPY
     public void givenAnyMoodMessage_WhenAnalysed_MessageShouldReturnHappy() {
         MoodAnalyser checkMood = new MoodAnalyser("I am in Any Mood");
         String mood = checkMood.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
 
- }
+    @Test
+    //IF Null Mood or no message then it should return HAPPY
+    public void givenMessage_whennull_ShouldReturnHappy() {
+        MoodAnalyser checkMood = new MoodAnalyser(null);
+        String mood = checkMood.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
