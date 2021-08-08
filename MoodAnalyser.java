455d53e2051549df24ecbd40b07a80030a7d0edb
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-08 18:24:20 +0530

    Take the mood message in Constructor

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class MoodAnalyser {
-    public String moodAnalyse(String message) {
+    public String message;
+
+    //Parameterised Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    //Default Constructor
+    MoodAnalyser() {
+    }
+
+    public String analyseMood() {
         if (message.contains("Sad")) {
             System.out.println("SAD");
             return "SAD";
